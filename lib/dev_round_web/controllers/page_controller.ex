fa916465477c30609a78843c20602b6211e92061
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2025-07-22 22:11:59 +0200

    Homepage: Redirect To Event Listing

--- lib/dev_round_web/controllers/page_controller.ex
+++ lib/dev_round_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule DevRoundWeb.PageController do
   use DevRoundWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home)
+    redirect(conn, to: ~p"/events")
   end
 end
