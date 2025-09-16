b70216f9167dcf4a60b93edbd3338639fa433861
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2025-09-16 21:50:55 +0200

    PDF Viewer: Re-work Error Handling
    A flash message was suboptimal and they cannot be issued by live
    components anyway.

--- lib/dev_round_web/components/event_slides_viewer_live.ex
+++ lib/dev_round_web/components/event_slides_viewer_live.ex
@@ -8,48 +8,57 @@ defmodule DevRoundWeb.EventSlidesViewerLive do
     ~H"""
     <div class="bg-base-300 text-base rounded-lg shadow-lg overflow-hidden my-8">
       <div id="pdf-container" class="relative">
-        <%= if @pdf_url do %>
-          <div
-            id="pdf-viewer"
-            phx-hook="PDFViewer"
-            data-pdf-url={@pdf_url}
-            data-pdf-page-number={@pdf_initial_page_number}
-            class="aspect-[16/9]"
-          >
-            <!-- PDF.js viewer will be rendered here -->
-            <div id="pdf-canvas-container" class="w-full">
-              <canvas id="pdf-canvas"></canvas>
-            </div>
-
-            <div
-              id="pdf-placeholder"
-              class="absolute top-0 flex items-center justify-center w-full aspect-[16/9] bg-base-300"
-            >
-              <div class="text-center">
-                <div class="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mx-auto mb-4">
-                </div>
-                <p>Loading PDF...</p>
-              </div>
+        <%= if @pdf_error do %>
+          <div class="flex items-center justify-center w-full aspect-[16/9] bg-base-300">
+            <div class="text-center opacity-70">
+              <img src={~p"/images/broken-heart.svg"} class="inline w-[128px]" />
+              <p class="mt-8">{@pdf_error}</p>
             </div>
           </div>
         <% else %>
-          <div class="flex items-center justify-center w-full aspect-[16/9] bg-base-300">
-            <p>
-              This event doesn't have any slides uploaded yet.
-            </p>
-          </div>
+          <%= if @pdf_url do %>
+            <div
+              id="pdf-viewer"
+              phx-hook="PDFViewer"
+              data-pdf-url={@pdf_url}
+              data-pdf-page-number={@pdf_initial_page_number}
+              class="aspect-[16/9]"
+            >
+              <!-- PDF.js viewer will be rendered here -->
+              <div id="pdf-canvas-container" class="w-full">
+                <canvas id="pdf-canvas"></canvas>
+              </div>
+
+              <div
+                id="pdf-placeholder"
+                class="absolute top-0 flex items-center justify-center w-full aspect-[16/9] bg-base-300"
+              >
+                <div class="text-center">
+                  <div class="animate-spin rounded-full h-12 w-12 border-b-2 border-primary mx-auto mb-4">
+                  </div>
+                  <p class="opacity-70">Loading PDF...</p>
+                </div>
+              </div>
+            </div>
+          <% else %>
+            <div class="flex items-center justify-center w-full aspect-[16/9] bg-base-300">
+              <p>
+                This event doesn't have any slides uploaded yet.
+              </p>
+            </div>
+          <% end %>
         <% end %>
         <!-- PDF Controls -->
         <div class="p-4 flex items-center justify-between bg-neutral">
           <div class="flex items-center space-x-4">
-            <button id="prev-page" class="btn btn-primary" disabled={!@pdf_url}>
+            <button id="prev-page" class="btn btn-primary" disabled={!@pdf_url || @pdf_error}>
               Previous
             </button>
-            <button id="next-page" class="btn btn-primary" disabled={!@pdf_url}>
+            <button id="next-page" class="btn btn-primary" disabled={!@pdf_url || @pdf_error}>
               Next
             </button>
           </div>
-          <div :if={@pdf_url} class="flex items-center space-x-4">
+          <div :if={@pdf_url && !@pdf_error} class="flex items-center space-x-4">
             <span class="text-sm">
               Page <span id="current-page">1</span> of <span id="total-pages">-</span>
             </span>
@@ -71,13 +80,6 @@ defmodule DevRoundWeb.EventSlidesViewerLive do
   end
 
   @impl true
-  def handle_event("pdf_error", %{"error" => error}, socket) do
-    {:noreply,
-     socket
-     |> put_flash(:error, "Error loading PDF: #{error}")}
-  end
-
-  @impl true
   def handle_info(
         %{topic: "event_slides", payload: %{url: url, page_number: page_number}},
         socket
@@ -90,6 +92,13 @@ defmodule DevRoundWeb.EventSlidesViewerLive do
   def handle_info(%{topic: "lectures", payload: %{url: url, page_number: page_number}}, socket)
       when url == socket.assigns.pdf_url do
     {:noreply, push_event(socket, "pdf_viewer_page_turn", %{pageNumber: page_number})}
+  end
+
+  @impl true
+  def handle_event("pdf_error", %{"error" => error}, socket) do
+    {:noreply,
+     socket
+     |> assign(:pdf_error, error)}
   end
 
   @impl true
@@ -106,9 +115,11 @@ defmodule DevRoundWeb.EventSlidesViewerLive do
 
   defp assign_pdf_fields(socket) do
     %{event: event} = socket.assigns
+
     socket
     |> assign(:pdf_url, get_pdf_url(event.slides_filename))
     |> assign(:pdf_initial_page_number, event.slides_page_number)
+    |> assign_new(:pdf_error, fn -> nil end)
   end
 
   defp get_pdf_url(slides_filename) when is_binary(slides_filename) do
