67b928be3e7c82a994364560dbb9870a06122e68
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2025-12-13 20:58:12 +0100

    Remove Dead Code

--- lib/dev_round_web/components/event_session_teams_slide_live.ex
+++ lib/dev_round_web/components/event_session_teams_slide_live.ex
@@ -13,25 +13,10 @@ defmodule DevRoundWeb.EventSessionTeamsSlideLive do
     socket =
       socket
       |> assign(assigns)
-      |> assign_time_remaining(event_session)
       |> assign_dates(event_session)
       |> assign_teams(event_session)
 
-    # Schedule tick aligned to wallclock seconds if live
-    if event_session.live and connected?(socket) do
-      schedule_next_tick(event_session.id)
-    end
-
     {:ok, socket}
-  end
-
-  defp schedule_next_tick(session_id) do
-    now = DateTime.utc_now()
-    # Calculate milliseconds until next full second
-    ms_until_next_second = 1000 - (now.microsecond |> elem(0) |> div(1000))
-
-    Process.send_after(self(), {:tick, session_id}, ms_until_next_second)
-    :ok
   end
 
   @impl true
@@ -83,20 +68,6 @@ defmodule DevRoundWeb.EventSessionTeamsSlideLive do
     |> assign_new(:teams, fn -> Hosting.list_teams_for_session(event_session) end)
   end
 
-  defp assign_time_remaining(socket, event_session) do
-    if event_session.live do
-      case calculate_time_remaining(event_session.actual_begin, event_session.end) do
-        {:ok, time_remaining} ->
-          assign(socket, :time_remaining, time_remaining)
-
-        {:expired, _} ->
-          assign(socket, :time_remaining, nil)
-      end
-    else
-      assign(socket, :time_remaining, nil)
-    end
-  end
-
   defp assign_dates(socket, event_session) do
     {:ok, now} = DateTime.now(Formats.time_zone())
 
@@ -113,34 +84,6 @@ defmodule DevRoundWeb.EventSessionTeamsSlideLive do
         :time,
         Formats.format_datetime_range(event_session.begin_local, event_session.end_local)
       )
-    end
-  end
-
-  defp calculate_time_remaining(begin_datetime, end_datetime) do
-    now = DateTime.utc_now()
-
-    case DateTime.compare(end_datetime, now) do
-      :gt ->
-        diff = DateTime.diff(end_datetime, now, :second)
-
-        days = div(diff, 86400)
-        hours = div(rem(diff, 86400), 3600)
-        minutes = div(rem(diff, 3600), 60)
-        seconds = rem(diff, 60)
-
-        total_days = DateTime.diff(end_datetime, begin_datetime, :day)
-
-        day_digits =
-          case total_days do
-            0 -> 0
-            _ -> total_days |> Integer.to_string() |> String.length()
-          end
-
-        {:ok,
-         %{days: days, hours: hours, minutes: minutes, seconds: seconds, day_digits: day_digits}}
-
-      _ ->
-        {:expired, %{days: 0, hours: 0, minutes: 0, seconds: 0, day_digits: 0}}
     end
   end
 end
