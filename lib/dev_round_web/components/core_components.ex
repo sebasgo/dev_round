5ecc6395ab8dfea06320cfb759887d3073758f0d
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2026-01-19 21:14:51 +0100

    Phx 1.8: Kill Back Component

--- lib/dev_round_web/components/core_components.ex
+++ lib/dev_round_web/components/core_components.ex
@@ -538,26 +538,6 @@ defmodule DevRoundWeb.CoreComponents do
     """
   end
 
-  @doc """
-  Renders a back navigation link.
-
-  ## Examples
-
-      <.back navigate={~p"/posts"}>Back to posts</.back>
-  """
-  attr :navigate, :any, required: true
-  slot :inner_block, required: true
-
-  def back(assigns) do
-    ~H"""
-    <div class="mt-16">
-      <.link navigate={@navigate} class="text-sm font-semibold leading-6 hover:opacity-75">
-        <.icon name="hero-arrow-left-solid" class="h-3 w-3" />
-        {render_slot(@inner_block)}
-      </.link>
-    </div>
-    """
-  end
 
   @doc """
   Renders a [Heroicon](https://heroicons.com).
