cac5b6506411742fd32d2119fd0dba68dba2773a
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2026-01-26 21:28:34 +0100

    Admin: Show User Roles

--- lib/dev_round_web/admin/user.ex
+++ lib/dev_round_web/admin/user.ex
@@ -41,10 +41,33 @@ defmodule DevRoundWeb.Admin.User do
         label: "Full Name",
         readonly: true
       },
+      role: %{
+        module: Backpex.Fields.Number,
+        label: "Role",
+        readonly: true,
+        only: [:index, :show],
+        render: &render_role/1
+      },
       experience_level: %{
         module: Backpex.Fields.Number,
         label: "Experience Level"
       }
     ]
+  end
+
+  defp render_role(%{value: :user} = assigns) do
+    ~H"""
+    <div class="badge badge-sm badge-neutral">
+      User
+    </div>
+    """
+  end
+
+  defp render_role(%{value: :admin} = assigns) do
+    ~H"""
+    <div class="badge badge-sm badge-primary">
+      Admin
+    </div>
+    """
   end
 end
