0f9074daabd8042b24539cf6cde0dc96c5edfb49
Sebastian Gottfried <sebastian.gottfried@posteo.de>
2026-07-27 12:02:02 +0200

    Fix Warnings

--- lib/dev_round_web/components/event_components.ex
+++ lib/dev_round_web/components/event_components.ex
@@ -181,8 +181,7 @@ defmodule DevRoundWeb.EventComponents do
     ~H"""
     <div class="badge badge-info flex items-center gap-2">
       <span class="relative flex h-3 w-3">
-        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-error opacity-75">
-        </span>
+        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-error opacity-75"></span>
         <span class="relative inline-flex rounded-full h-3 w-3 bg-error"></span>
       </span>
       <span class="font-semibold uppercase mt-px">Live</span>
